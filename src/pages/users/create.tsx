835947e247b337af02416c8f950548e573b60f27
Gerson Rocha <gersonrocha9@gmail.com>
2022-08-22 15:01:23 -0300

    create new user page done

---
+++ src/pages/users/create.tsx
@@ -0,0 +1,50 @@
+import { Box, Button, Divider, Flex, Heading, HStack, Icon, SimpleGrid, VStack } from "@chakra-ui/react";
+import { RiDeleteBack2Line, RiSaveLine } from "react-icons/ri";
+import { Input } from "../../components/Form/Input";
+
+import { Header } from "../../components/Header";
+
+import Sidebar from "../../components/Sidebar";
+
+export default function CreateUser() {
+  return (
+    <Box>
+      <Header />
+      <Flex w="100%" my={6} maxW={1480} mx="auto" px={6}>
+        <Sidebar />
+
+        <Box flex={1} borderRadius={8} bg="gray.800" p={8}>
+          <Heading as="h2" size="lg" fontWeight="normal">
+            Criar usuário
+          </Heading>
+
+          <Divider my={6} borderColor="gray.700" />
+
+          <VStack spacing={8}>
+            <SimpleGrid minChildWidth="240px" spacing={8} w="100%">
+              <Input name="name" label="Nome Completo" placeholder="Nome Completo" />
+              <Input name="email" label="E-mail" placeholder="E-mail" />
+              <Input name="password" label="Senha" placeholder="Senha" />
+              <Input
+                name="password_confirmation"
+                label="Digite a senha novamente"
+                placeholder="Digite a senha novamente"
+              />
+            </SimpleGrid>
+          </VStack>
+
+          <Flex mt={8} justify="flex-end" align="center">
+            <HStack spacing={4}>
+              <Button colorScheme="whiteAlpha" leftIcon={<Icon as={RiDeleteBack2Line} fontSize={20} />}>
+                Cancelar
+              </Button>
+              <Button colorScheme="pink" leftIcon={<Icon as={RiSaveLine} fontSize={20} />}>
+                Salvar
+              </Button>
+            </HStack>
+          </Flex>
+        </Box>
+      </Flex>
+    </Box>
+  );
+}
