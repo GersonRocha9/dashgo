7eb922114945acb729698cccc55cb77f3bee932d
Gerson Rocha <gersonrocha9@gmail.com>
2022-08-22 11:14:55 -0300

    sign in page and form component done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,21 @@
-export default function Home() {
+import { Button, Flex, Stack, Text } from "@chakra-ui/react";
+import { Input } from "../components/Form/Input";
+
+export default function SignIn() {
   return (
-    <div>
-      <h1>Hello Dashgo</h1>
-    </div>
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex as="form" w="100%" maxW={400} bg="gray.800" borderRadius={8} p={8} flexDir="column">
+        <Text fontSize="3xl" fontWeight="bold" textAlign="center" mb={8}>
+          Login
+        </Text>
+        <Stack spacing={4}>
+          <Input name="email" label="E-mail" type="email" />
+          <Input name="password" label="Password" type="password" />
+        </Stack>
+        <Button type="submit" mt={4} colorScheme="pink" size="lg">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
   );
 }
