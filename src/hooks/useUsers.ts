ed10a7126caa5a9d2d75a6a6c86b14688e88d730
Gerson Rocha <gersonrocha9@gmail.com>
2022-08-23 15:11:57 -0300

    fix type bug on data

--- src/hooks/useUsers.ts
+++ src/hooks/useUsers.ts
@@ -2,6 +2,13 @@ import { useQuery } from "@tanstack/react-query";
 
 import { getUsers } from "../helpers/getUsers";
 
+type User = {
+  id: string;
+  name: string;
+  email: string;
+  createdAt: string;
+};
+
 export function useUsers() {
-  return useQuery(["users"], getUsers, { staleTime: 1000 * 5 });
+  return useQuery<User[]>(["users"], getUsers, { staleTime: 1000 * 5 });
 }
